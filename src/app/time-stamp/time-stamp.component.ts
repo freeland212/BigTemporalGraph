2fce4538ffed21edab6263c30854671a2610685e
irmlerjo <jonas_irmler@online.de>
2020-06-26 13:20:13 +0200

    Fixed Date Component

--- src/app/time-stamp/time-stamp.component.ts
+++ src/app/time-stamp/time-stamp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-time-stamp',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./time-stamp.component.scss']
 })
 export class TimeStampComponent implements OnInit {
+
+
+  @ViewChild('picker') picker: any;
 
   constructor() { }
 
